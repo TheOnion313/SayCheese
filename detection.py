39058d8004e9ef444913caab0d02b176c767dfc6
TheOnion313 <royn77777@gmail.com>
2021-05-12 14:37:00 +0300

    Changed detection concept now the detect method is free and not bound by ranges

--- detection.py
+++ detection.py
@@ -6,27 +6,16 @@ import cv2
 
 class Detection(ABC):
 
-    def __init_subclass__(cls, **kwargs):
-        assert 'relations' in kwargs.keys()
-        cls.relations = kwargs['relations']
-
-    def __iter__(self):
-        return self.relations
-
     @abstractmethod
     def relations_from_encoding(self, encoding: Iterable[float]) -> Union[float, Iterable[float]]:
         pass
 
-    def detect(self, encoding: Iterable[float]) -> bool:
-        inp = self.relations_from_encoding(encoding)
-        out = self.relations
+    @abstractmethod
+    def detect(self, encoding: Iterable[Tuple[float]]) -> bool:
+        pass
 
-        if type(inp) == Iterable:
-            return all(map(lambda i: inp[i] in out[i], range(len(inp))))
-        return inp in out
-
-    def __call__(self, encoding):
-        return self.detect(encoding)
+    def __call__(self, *args, **kwargs):
+        return self.detect(*args, **kwargs)
 
     @abstractmethod
     def calibrate(self, me: Union[Iterable[float], Iterable[Iterable[float]]]):
